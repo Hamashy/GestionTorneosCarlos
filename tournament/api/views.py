72cb37f1fa7f273032bb7d20a0ad686ae84fa960
carlos <carloscarlangasxd@gmail.com>
2023-11-10 18:34:27 -0500

    def 2

--- tournament/api/views.py
+++ tournament/api/views.py
@@ -11,18 +11,18 @@ from tournament.models import Equipo, Partida, Torneo
 class EquipoApiViewSet(ModelViewSet):
     queryset = Equipo.objects.all()
     serializer_class = EquipoSerializer
-    permission_classes = [IsAdminUser]
+    #permission_classes = [IsAdminUser]
 
 
 class TorneoApiViewSet(ModelViewSet):
     queryset = Torneo.objects.all()
     serializer_class = TorneoSerializer
-    permission_classes = [IsAdminUser]
+   # permission_classes = [IsAdminUser]
 
 
 class PartidaApiViewSet(ModelViewSet):
     queryset = Partida.objects.all()
     serializer_class = PartidaSerializer
-    permission_classes = [IsAdminUser]
+    #permission_classes = [IsAdminUser]
 
 
