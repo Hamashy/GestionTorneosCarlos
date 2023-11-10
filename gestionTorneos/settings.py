b50cd7e741897496faff804c6345e03e3021a557
carlos <carloscarlangasxd@gmail.com>
2023-11-10 17:20:35 -0500

    config render

--- gestionTorneos/settings.py
+++ gestionTorneos/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 from datetime import timedelta
 
+import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,12 +25,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)l0f9&0+@7b(x^66@6nvlrmcyd#f=f!&^crok(pk3%olltij3'
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -50,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,14 +102,9 @@ WSGI_APPLICATION = 'gestionTorneos.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Usamos el motor de base de datos MySQL
-        'NAME': 'gestionTorneosDB',  # Nombre de tu base de datos MySQL
-        'USER': 'root',            # Usuario de MySQL
-        'PASSWORD': '',     # Contraseña de MySQL
-        'HOST': 'localhost',                   # Host donde se encuentra tu base de datos MySQL
-        'PORT': '3307',                            # Puerto de la base de datos MySQL (deja en blanco para el valor predeterminado)
-    }
+    'default': dj_database_url.config(
+        default='mysql://root:@localhost:3307/gestionTorneosDB',     
+        conn_max_age=600    )
 }
 
 
@@ -142,6 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
