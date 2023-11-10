a67306aeeb54b1804802275e8a3f458a34482dd9
carlos <carloscarlangasxd@gmail.com>
2023-11-10 16:24:54 -0500

    requirements si son

--- gestionTorneos/urls.py
+++ gestionTorneos/urls.py
@@ -44,7 +44,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('', admin.site.urls),
+   # path('', admin.site.urls),
     path('admin/', admin.site.urls),\
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
