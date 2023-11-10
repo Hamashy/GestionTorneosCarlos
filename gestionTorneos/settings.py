8167a0fa6153fea455bcb41320473b61554ace3b
carlos <carloscarlangasxd@gmail.com>
2023-11-10 17:54:17 -0500

    def

--- gestionTorneos/settings.py
+++ gestionTorneos/settings.py
@@ -101,10 +101,14 @@ WSGI_APPLICATION = 'gestionTorneos.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='mysql://root:@localhost:3307/gestionTorneosDB',     
+#         conn_max_age=600    )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='mysql://root:@localhost:3307/gestionTorneosDB',     
-        conn_max_age=600    )
+    'default': dj_database_url.config()
 }
 
 
